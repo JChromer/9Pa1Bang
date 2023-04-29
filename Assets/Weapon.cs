c5d35c3f2cca3d289a8d07463827564bb0adffdd
JChromer <jwCodeManager@gmail.com>
2023-04-29 11:30:52 +0900

    웨폰 시스템 시작

---
+++ Assets/Weapon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int level = 1;
+    public float moveSpeed = 10f;
+    public float lifeTime = 1f;
+    public float interval = 1f;
+    public Vector3 weaponScale = Vector3.one;
+}
